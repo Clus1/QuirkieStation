54104a252a0ab614db2b4c08acb82fad37809257
Jason McCutchan <jason.mccutchan@gmail.com>
2022-05-01 19:26:14 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import logo from './example.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          So what is QuirkieStation going to look like?
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
